e342d030c8e593c36f42025f8ea29587af1123f7
repl.it user <replituser@example.com>
2020-10-02 21:53:27 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -94,10 +94,10 @@ void removeNode(TreeMap * tree, TreeNode* node) {
   }else if((node ->left != NULL) && (node ->right != NULL)){
     printf("Xd\n");
   }else{
-    printf("\(node ->right)");
     tree -> current = node -> parent;
     tree -> current -> parent = node -> right;
-      
+    
+      printf("%p",node -> right -> key);
   
   }
 
