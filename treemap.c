d5c3f43f2fd69e0fb9bdfce447ecf426e06bb2be
repl.it user <replituser@example.com>
2020-10-03 17:23:03 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -170,7 +170,7 @@ void * upperBound(TreeMap * tree, void* key) {
 // Implemente las funciones para recorrer la estructura: void firstTreeMap(TreeMap tree) retorna el primer valor del mapa (el menor). void nextTreeMap(TreeMap tree) retornar el siguiente valor del mapa a partir del puntero TreeNode* current. Recuerde actualizar este puntero.
 void * firstTreeMap(TreeMap * tree) {
   TreeNode * node;
-  while(node -> left != NULL){
+  while(node -> right != NULL){
     node = node -> right;
 
   }
