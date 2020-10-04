f282772f3255819c2f1750b0fc1e4445ce10b8f8
repl.it user <replituser@example.com>
2020-10-04 00:52:29 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -182,7 +182,7 @@ void * upperBound(TreeMap * tree, void* key) {
       aux = aux -> right;
     }
     if (1==tree -> lower_than(key,aux -> key)||(is_equal(tree,aux -> key,key))){
-      printf("aaaa");
+      printf("a");
       if ((ub_node == NULL)||(tree -> lower_than(aux ->key ,ub_node))){
         ub_node = aux;
       }
